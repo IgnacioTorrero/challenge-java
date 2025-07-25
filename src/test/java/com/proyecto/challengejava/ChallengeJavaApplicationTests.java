b00e0fefab75ea2b8a87b78de8c1e920a806bccc
Ignacio Torrero <ignacio_torrero97@hotmail.com>
2025-07-25 12:26:23 -0300

    github actions 04

--- src/test/java/com/proyecto/challengejava/ChallengeJavaApplicationTests.java
+++ src/test/java/com/proyecto/challengejava/ChallengeJavaApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(classes = com.proyecto.challengejava.ChallengeJavaApplication.class)
+@SpringBootTest
 @ActiveProfiles("test")
 class ChallengeJavaApplicationTests {
 
